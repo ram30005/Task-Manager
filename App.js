694c614c044567360763c70d8fca86ba536fb3bb
Ram Prasaanth <100895346+ram30005@users.noreply.github.com>
2024-07-19 10:23:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import FunctionTest from './ComponentsTest/TaskManager.js';
+ 
+
+/* import ListView from './ComponentsTest/ListView'
+ */
+/* import CentralComponent from './ComponentsTest/CentralComponent' */
+function App() {
+  
+  return (  
+    <>     
+      <FunctionTest/>
+      {/*  <ListView /> */}
+    </>   
+   
+  )
+}
+export default App;
---
+++ TaskManager.js
@@ -0,0 +1,55 @@
+import {useState} from 'react'
+
+function FunctionTest()
+{
+    const[tasksList,setTasksList] = useState([{}])
+    const[task,setTask] = useState({id:0,name:'',toDo:'',lastDate:''})
+    let id=0;
+    function addTask()
+    {
+        const temp= [...tasksList,task];
+        setTasksList(temp);
+        setTask({name:'',toDo:'',lastDate:''})
+    }
+    function taskHandler(e)
+    {
+        setTask(
+            {
+                ...task,
+                [e.target.name] : e.target.value
+            }
+        )
+    }
+    return(
+        <>
+            <input type="text" value={task.name} placeholder="Enter Name" name="name" onChange={taskHandler} />
+            <br/>
+            <input type="text" value={task.toDo} placeholder="Enter To Do" name="toDo" onChange={taskHandler} />
+            <br/>
+            <input type="text" value={task.lastDate} placeholder="Last day to complete the task" name="lastDate" onChange={taskHandler} />
+            <br/>
+            <button onClick={addTask}>Add Task</button>
+
+            <h2>Task Are:</h2>
+            <ol>
+                { tasksList.map(t=>{
+                    if(t.name!==undefined)
+                    {
+                        return(
+                        <div key={id++}>
+                            <li>{t.name}</li> 
+                            <ul>
+                                <li>{t.toDo}</li>
+                                <p>Last Date to Complete te Task: <li>{t.lastDate}</li></p>
+                            </ul>
+                            
+                        </div>)
+                    }
+                    return ''
+                }
+                    )}
+            </ol>
+        </>
+    )
+}
+export default FunctionTest;
